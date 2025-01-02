78527c703b9f2a7445111c382909127e3d7ca1da
Diego Chagas <drochachagas@gmail.com>
2025-01-01 21:37:14 -0300

    update data

--- src/app/news/[id]/content.tsx
+++ src/app/news/[id]/content.tsx
@@ -1,36 +1,49 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { useTranslations } from 'next-intl'
-import Error from '@/app/components/error'
-import Icon from '@/app/components/icons'
-import { NewsProps } from '@/pages/api/news'
-import { getClothName } from '@/helpers'
+import Image from "next/image";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+import Error from "@/app/components/error";
+import Icon from "@/app/components/icons";
+import { NewsProps } from "@/pages/api/news";
+import { getClothName } from "@/helpers";
 
 interface ContentProps {
-  data: NewsProps
-  error: any
-  url: string
-  isBrazil?: boolean
+  data: NewsProps;
+  error: any;
+  url: string;
+  isBrazil?: boolean;
 }
 
 export default function Content({ data, error, url, isBrazil }: ContentProps) {
-  const t = useTranslations()
+  const t = useTranslations();
 
   const renderListItem = (title: string, description: any) => (
     <li className="list-disc">
       <h6 className="font-bold text-lg">{t(title)}</h6>
       {description?.id ? (
         description?.site ? (
-          <a className="hover:font-bold" href={description.site} target="_blank" rel="noopener noreferrer">{description.name}</a>
-        ) : description.name
-      ) : description}
+          <a
+            className="hover:font-bold"
+            href={description.site}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {description.name}
+          </a>
+        ) : (
+          description.name
+        )
+      ) : (
+        description
+      )}
     </li>
-  )
+  );
 
   return (
     <div className="my-28 md:my-48 w-full flex justify-center flex-col items-center">
       <div className="flex w-full max-w-7xl">
-        <h1 className="uppercase font-extrabold text-6xl md:text-8xl">{t('news')}</h1>
+        <h1 className="uppercase font-extrabold text-6xl md:text-8xl">
+          {t("news")}
+        </h1>
       </div>
 
       <div className="flex flex-col justify-center relative bg-zinc-100 w-full my-5 py-10 px-6 items-center">
@@ -38,118 +51,174 @@ export default function Content({ data, error, url, isBrazil }: ContentProps) {
           <div className="flex flex-col w-full">
             {!error && data.date ? (
               <>
-                <span className="font-bold text-lg block my-5">{data.date}</span>
-                
+                <span className="font-bold text-lg block my-5">
+                  {data.date}
+                </span>
+
                 <div className="flex flex-wrap gap-4 items-end">
                   <div className="flex flex-col">
                     <span className="font-bold text-xl text-white uppercase bg-black px-10 py-0.5">
                       {t(data.saint.history.midia.name)}
                     </span>
                   </div>
-                  {data.saint.history.midia.name !== data.saint.cloth.history.midia?.name && (
+                  {data.saint.history.midia.name !==
+                    data.saint.cloth.history.midia?.name && (
                     <div className="flex flex-col md:items-center">
-                      <h2 className="text-3xl font-extrabold uppercase">{t('cloth')}</h2>
+                      <h2 className="text-3xl font-extrabold uppercase">
+                        {t("cloth")}
+                      </h2>
                       <span className="font-bold text-xl text-white uppercase bg-black px-10 py-0.5">
                         {t(data.saint.cloth.history.midia?.name)}
                       </span>
                     </div>
                   )}
                 </div>
-                
-                <h2 className="mt-5 text-3xl font-extrabold">{data.saint.character.name}</h2>
-                
+
+                <h2 className="mt-5 text-3xl font-extrabold">
+                  {data.saint?.character?.name}
+                </h2>
+
                 <div className="bg-neutral-400 my-5 md:my-10 p-4 md:p-8">
-                  <h1 className="text-2xl font-bold">{t(data.saint.history.name)}</h1>
+                  <h1 className="text-2xl font-bold">
+                    {t(data.saint.history.name)}
+                  </h1>
                 </div>
-                
+
                 <p>{t(`newsDescription.${data.saint.id}`)}</p>
-                
+
                 <figure className="flex flex-col items-center my-5">
-                  <Image className="border-2 border-black" src={data.saint.image} width={536} height={400} alt={t('saintClothScheme')} />
-                
+                  <Image
+                    className="border-2 border-black"
+                    src={data.saint.image}
+                    width={536}
+                    height={400}
+                    alt={t("saintClothScheme")}
+                  />
+
                   <figcaption>
-                    <small className="font-semibold capitalize">{getClothName(t, data.saint.cloth?.name)}</small>
+                    <small className="font-semibold capitalize">
+                      {getClothName(t, data.saint.cloth?.name)}
+                    </small>
                   </figcaption>
                 </figure>
-                  
+
                 <ul className="flex flex-col gap-2 ml-4 mt-10">
-                  {renderListItem('god', data.saint.god?.name)}
-                  {renderListItem('class', t(data.saint.group.class))}
-                  {renderListItem('rank', t(data.saint.rank) || `${t('unknown')} ${t('rank')}`)}
+                  {renderListItem("god", data.saint.god?.name)}
+                  {renderListItem("class", t(data.saint.group.class))}
+                  {renderListItem("rank", t(data.saint.rank || "unknown"))}
                   {data.saint.artist && data.saint.cloth.artist ? (
-                    data.saint.artist.id === data.saint.cloth.artist.id ? 
-                      renderListItem('schemeBy', data.saint.artist)
-                    : (
+                    data.saint.artist.id === data.saint.cloth.artist.id ? (
+                      renderListItem("schemeBy", data.saint.artist)
+                    ) : (
                       <>
-                        {data.saint.artist && renderListItem('characterBy', data.saint.artist)}
-                        {data.saint.cloth.artist && renderListItem('clothBy', data.saint.cloth.artist)}
+                        {data.saint.artist &&
+                          renderListItem("characterBy", data.saint.artist)}
+                        {data.saint.cloth.artist &&
+                          renderListItem("clothBy", data.saint.cloth.artist)}
                       </>
                     )
                   ) : (
-                    <>  
-                      {data.saint.artist && renderListItem('characterBy', data.saint.artist)}
-                      {data.saint.cloth.artist && renderListItem('clothBy', data.saint.cloth.artist)}
-                      {!data.saint.artist && !data.saint.cloth.artist && renderListItem('schemeBy', `${t('unknown')} (${t('artist')})`)}
+                    <>
+                      {data.saint.artist &&
+                        renderListItem("characterBy", data.saint.artist)}
+                      {data.saint.cloth.artist &&
+                        renderListItem("clothBy", data.saint.cloth.artist)}
+                      {!data.saint.artist &&
+                        !data.saint.cloth.artist &&
+                        renderListItem("schemeBy", t("unknown"))}
                     </>
                   )}
                 </ul>
 
                 {isBrazil && data.amazon && (
                   <div className="my-16">
-                    <a className="hover:font-bold cursor-pointer text-lg flex flex-col" href={data.amazon} target="_blank" rel="noopener noreferrer">
+                    <a
+                      className="hover:font-bold cursor-pointer text-lg flex flex-col"
+                      href={data.amazon}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <b className="inline-flex items-center gap-1">
                         <Icon name="arrow-right" size={15} />
                         <span className="uppercase">Clique aqui!</span>
                       </b>
-                      <span className="">E compre a história onde esse personagem apareceu!</span>
+                      <span className="">
+                        E compre a história onde esse personagem apareceu!
+                      </span>
                     </a>
                   </div>
                 )}
 
                 <div className="relative self-end">
-                  <h3 className="absolute top-9 md:top-20 -left-1 font-extrabold text-2xl md:text-5xl -rotate-6 uppercase">{t('share')}</h3>
-                  
-                  <Image className="w-72 md:w-[570px]" src="/banners/bg_share.png" alt={t('shareImage')} width={1044} height={398} />
-                  
+                  <h3 className="absolute top-9 md:top-20 -left-1 font-extrabold text-2xl md:text-5xl -rotate-6 uppercase">
+                    {t("share")}
+                  </h3>
+
+                  <Image
+                    className="w-72 md:w-[570px]"
+                    src="/banners/bg_share.png"
+                    alt={t("shareImage")}
+                    width={1044}
+                    height={398}
+                  />
+
                   <a
                     className="absolute -bottom-2 left-16 md:left-32 w-12 h-12 md:w-20 md:h-20 rounded-full border-4 border-black flex items-center justify-center bg-white group"
                     href={`http://www.facebook.com/share.php?u=${url}`}
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <Icon className="w-5 md:w-10 md:group-hover:w-12 h-auto" name="facebook" />
+                    <Icon
+                      className="w-5 md:w-10 md:group-hover:w-12 h-auto"
+                      name="facebook"
+                    />
                   </a>
-    
+
                   <a
                     className="absolute -bottom-2 left-32 md:left-60 w-12 h-12 md:w-20 md:h-20 rounded-full border-4 border-black flex items-center justify-center bg-white group"
-                    href={`http://twitter.com/share?text=${encodeURI(`${data.saint.character.name} - ${t(data.saint.history.midia.name)}: ${t(data.saint.history.name)}`)}+%23saintseiya %23clothmyth %23clothmythex %23actionfigures %23cavaleirosdozodiaco %23caballerosdelzodiaco %23leschevaliersduzodiaque %23icavalieridellozodiaco %23bandai %23tamashii %23cdz&url=${url}`}
+                    href={`http://twitter.com/share?text=${encodeURI(
+                      `${data.saint.character.name} - ${t(
+                        data.saint.history.midia.name
+                      )}: ${t(data.saint.history.name)}`
+                    )}+%23saintseiya %23clothmyth %23clothmythex %23actionfigures %23cavaleirosdozodiaco %23caballerosdelzodiaco %23leschevaliersduzodiaque %23icavalieridellozodiaco %23bandai %23tamashii %23cdz&url=${url}`}
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <Icon className="w-5 md:w-10 md:group-hover:w-12 h-auto" name="x" />
+                    <Icon
+                      className="w-5 md:w-10 md:group-hover:w-12 h-auto"
+                      name="x"
+                    />
                   </a>
                 </div>
               </>
             ) : (
               <Error>
                 <figure className="flex flex-col items-center gap-10 p-5 md:p-10">
-                  <Image className="w-full max-w-72" src="/banners/sorry.jpg" width={532} height={984} alt={t('errorMangaImage')} />
-                  <figcaption className="text-xs sm:text-base">{t('newsNotFound')}</figcaption>
+                  <Image
+                    className="w-full max-w-72"
+                    src="/banners/sorry.jpg"
+                    width={532}
+                    height={984}
+                    alt={t("errorMangaImage")}
+                  />
+                  <figcaption className="text-xs sm:text-base">
+                    {t("newsNotFound")}
+                  </figcaption>
                 </figure>
               </Error>
             )}
-              
+
             <div className="mt-20 mb-20 mx-auto flex justify-center md:justify-end">
               <Link
                 className="block text-center w-64 font-extrabold text-yellow-500 hover:text-black uppercase bg-black hover:bg-yellow-500 py-3"
                 href="/news"
               >
-                {t('backToNewsList')}
+                {t("backToNewsList")}
               </Link>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/api/news/index.ts
+++ src/pages/api/news/index.ts
@@ -1,52 +1,68 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import saintsJson from '../data/saints.json'
-import midiasJson from '../data/midias.json'
-import newsJson from '../data/news.json'
-import { getContentByPage, loadSaintData, SaintProps } from '../classes'
+import type { NextApiRequest, NextApiResponse } from "next";
+import saintsJson from "../data/saints.json";
+import midiasJson from "../data/midias.json";
+import newsJson from "../data/news.json";
+import { getContentByPage, loadSaintData, SaintProps } from "../classes";
 
 export interface NewsProps {
-  saint: SaintProps
-  date: string
-  amazon?: string
+  saint: SaintProps;
+  date: string;
+  amazon?: string;
 }
 
-function filterNewsBySearchValueAndMidia(news: any[], searchValue: string, midia: any) {
-  return news.filter(item => {
-    if (midia && midia.id !== item.saint?.history?.midia?.id) return false
+function filterNewsBySearchValueAndMidia(
+  news: any[],
+  searchValue: string,
+  midia: any
+) {
+  return news.filter((item) => {
+    if (midia && midia.id !== item.saint?.history?.midia?.id) return false;
     return (
       item.date.includes(searchValue) ||
       item.saint?.character?.name?.toLowerCase().includes(searchValue) ||
-      item.saint?.cloth?.name?.some((name: string) => name.toLowerCase().includes(searchValue)) ||
+      item.saint?.cloth?.name?.some((name: string) =>
+        name.toLowerCase().includes(searchValue)
+      ) ||
       item.saint?.history?.midia?.name?.includes(searchValue)
-    )
-  })
+    );
+  });
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const { p, m, s } = req.query
-  const midia = midiasJson.find(midia => midia.id === m)
-  
+  const { p, m, s } = req.query;
+  const midia = midiasJson.find((midia) => midia.id === m);
+
   // TODO: REMOVE THIS FILTER WHEN THE NEWS OF THE SAINT 680 IS RELEASED
-  const newsDraft = newsJson.filter(item => !!item.date)
+  const newsDraft = newsJson.filter((item) => !!item.date);
   // TODO: REMOVE THIS FILTER WHEN THE NEWS OF THE SAINT 680 IS RELEASED
 
-  const news = newsDraft.map(item => {
-    const saint = saintsJson.find(saint => saint.id === item.saint)
-    return { ...item, date: new Date(item.date).toLocaleDateString(), saint: loadSaintData(saint) }
-  })
+  const news = newsDraft.map((item) => {
+    const saint = saintsJson.find((saint) => saint.id === item.saint);
+    return {
+      ...item,
+      date: new Date(item.date).toLocaleDateString(),
+      saint: loadSaintData(saint),
+    };
+  });
   if (s) {
-    const searchValue = s.toString().toLowerCase()
-    const filteredNews = filterNewsBySearchValueAndMidia(news, searchValue, midia)
-    res.status(200).json({ ...getContentByPage(filteredNews, p) })
+    const searchValue = s.toString().toLowerCase();
+    const filteredNews = filterNewsBySearchValueAndMidia(
+      news,
+      searchValue,
+      midia
+    );
+    res.status(200).json({ ...getContentByPage(filteredNews, p) });
   } else if (midia) {
-    const filteredNews = news.filter(item => item.saint.history?.midia?.id === midia.id)
-    res.status(200).json({ ...getContentByPage(filteredNews, p) })
+    const filteredNews = news.filter(
+      (item) => item.saint.history?.midia?.id === midia.id
+    );
+    res.status(200).json({ ...getContentByPage(filteredNews, p) });
   } else if (p) {
-    res.status(200).json({ ...getContentByPage(news, p) })
+    res.status(200).json({ ...getContentByPage(news, p) });
   } else {
-    res.status(200).json({ ...getContentByPage(news.slice(0, 10), p) })
+    res.status(200).json({ ...getContentByPage(news.slice(0, 10), p) });
   }
 }
