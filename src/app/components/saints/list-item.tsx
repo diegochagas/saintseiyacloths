5547dc4810356ea1421034c6cd89c6e842f7024f
Diego Chagas <drochachagas@gmail.com>
2025-01-09 00:02:56 -0300

    update data

--- src/app/components/saints/list-item.tsx
+++ src/app/components/saints/list-item.tsx
@@ -1,32 +1,42 @@
-import { useTranslations } from 'next-intl'
-import Image from 'next/image'
-import Link from 'next/link'
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import Link from "next/link";
 
 interface ListItemProps {
-  id?: string
-  image?: string
-  cloth?: string
-  name?: string
+  id?: string;
+  image?: string;
+  cloth?: string;
+  name?: string;
 }
 
-export default function ListItem({ id, image = '/cloth-schemes/others/no-scheme.jpg', cloth, name }: ListItemProps) {
-  const t = useTranslations()
+export default function ListItem({
+  id,
+  image = "/cloth-schemes/others/no-scheme.jpg",
+  cloth,
+  name,
+}: ListItemProps) {
+  const t = useTranslations();
 
   return (
-    <li className="flex flex-col items-center">
-      <Link className="w-full group" href={`/news/${id}`}>
+    <li>
+      <Link
+        className="w-full group flex flex-col items-center"
+        href={`/news/${id}`}
+      >
         <small className="capitalize font-bold">{cloth}</small>
         <figure className="h-28 overflow-hidden">
           <Image
             className="w-full h-full object-contain group-hover:scale-110 duration-300"
             src={image}
-            alt={t('saintClothScheme')}
+            alt={t("saintClothScheme")}
             width={542}
             height={400}
           />
         </figure>
-        <small className="font-semibold">{name ?? `${t('unknown')} ${t('character')}`}</small>
+        <small className="font-semibold">
+          {name ?? `${t("unknown")} ${t("character")}`}
+        </small>
       </Link>
     </li>
-  )
+  );
 }
